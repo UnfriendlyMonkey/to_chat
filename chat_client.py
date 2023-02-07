167f571d5711f6a3199f756eb3202fb23170d45d
UnfriendlyMonkey <andreysereda@ya.ru>
2023-02-08 00:47:51 +0300

    Step 3. Save message history to file

--- chat_client.py
+++ chat_client.py
@@ -1,16 +1,27 @@
 import asyncio
+import aiofiles
+import datetime
+
+
+def formatted_time():
+    now = datetime.datetime.now()
+    formatted_datetime = now.strftime('[%d.%m.%y %H:%M]')
+    return formatted_datetime
 
 
 async def tcp_echo_client():
     reader, writer = await asyncio.open_connection(
             host='minechat.dvmn.org', port=5000
     )
-    while True:
-        data = await reader.readline()
-        print(data)
-        message = data.decode()
-        if message:
-            print(message)
+    async with aiofiles.open('chatting.log', mode='a') as log_file:
+        await log_file.write(f'{formatted_time()} Установлено соединение\n')
+        while True:
+            # TODO: is there another way to make a loop?
+            data = await reader.readline()
+            message = data.decode()
+            if message:
+                print(message, end='')
+                await log_file.write(f'{formatted_time()} {message}')
     # TODO: Code is unreachable
     writer.close()
     await writer.wait_closed()
