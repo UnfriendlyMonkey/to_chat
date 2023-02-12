243eb3fd573b1a8741288c8bfaf4202813822740
UnfriendlyMonkey <andreysereda@ya.ru>
2023-02-12 08:53:20 +0300

    Add argparse to read CLA

--- chat_client.py
+++ chat_client.py
@@ -1,6 +1,8 @@
 import asyncio
 import aiofiles
 import datetime
+import argparse
+
 
 
 def formatted_time():
@@ -9,11 +11,11 @@ def formatted_time():
     return formatted_datetime
 
 
-async def tcp_echo_client():
+async def tcp_echo_client(host, port, history_file):
     reader, writer = await asyncio.open_connection(
-            host='minechat.dvmn.org', port=5000
+            host=host, port=port
     )
-    async with aiofiles.open('chatting.log', mode='a') as log_file:
+    async with aiofiles.open(history_file, mode='a') as log_file:
         await log_file.write(f'{formatted_time()} Установлено соединение\n')
         while True:
             # TODO: is there another way to make a loop?
@@ -27,4 +29,43 @@ async def tcp_echo_client():
     await writer.wait_closed()
 
 
-asyncio.run(tcp_echo_client())
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='''Local chat client.
+        Print messages to stdout and save them to file'''
+    )
+    parser.add_argument(
+        '-s',
+        '--host',
+        nargs='?',
+        default='minechat.dvmn.org',
+        help='host site to be connected to'
+    )
+    parser.add_argument(
+        '-p',
+        '--port',
+        type=int,
+        nargs='?',
+        default=5000,
+        help='host port to be connected to'
+    )
+    parser.add_argument(
+        '-y',
+        '--history',
+        nargs='?',
+        default='chatting.log',
+        help='file to write chat history to'
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_arguments()
+    host, port, history_file = args.host, args.port, args.history
+    asyncio.run(tcp_echo_client(host, port, history_file))
+
+
+if __name__ == '__main__':
+    main()
