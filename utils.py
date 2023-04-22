825ded295130bf9a2a39047af4faae27ccdf02b4
UnfriendlyMonkey <andreysereda@ya.ru>
2023-04-22 19:05:10 +0300

    Add reconnection in case of network errors

--- utils.py
+++ utils.py
@@ -1,16 +1,36 @@
 import asyncio
+import socket
 from contextlib import asynccontextmanager
+from typing import Tuple
+
+
+def increase_delay() -> int:
+    increasing_delays = [0.01, 0.1, 0.3, 0.7, 1.2, 2.5]
+    max_delay = 5
+    for delay in increasing_delays:
+        yield delay
+    while True:
+        yield max_delay
 
 
 @asynccontextmanager
-async def get_asyncio_connection(host: str, port: str) -> None:
-    reader, writer = await asyncio.open_connection(
-            host=host, port=port
-    )
+async def get_asyncio_connection(
+    host: str,
+    port: str
+) -> Tuple[asyncio.StreamReader, asyncio.StreamWriter]:
     try:
-        yield reader, writer
+        for delay in increase_delay():
+            try:
+                reader, writer = await asyncio.open_connection(
+                    host=host, port=port
+                )
+                yield reader, writer
+            except (
+                socket.gaierror,
+                ConnectionRefusedError,
+                ConnectionResetError
+            ):
+                await asyncio.sleep(delay)
     finally:
-        print('Closing connection')
         writer.close()
         await writer.wait_closed()
-        print('Connection closed')
